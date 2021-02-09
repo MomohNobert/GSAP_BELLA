ae05a07738546d762ce0bf60513732538143f6e4
Momoh Nobert <okhai0112@gmail.com>
2021-02-09 15:08:04 +0100

    added moving images functionality

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -43,7 +43,41 @@ function initHeaderTilt() {
 function moveImages(e) {
     const { offsetX, offsetY, target } = e;
     const { clientWidth, clientHeight } = target;
-    console.log(offsetX, offsetY, clientWidth, clientHeight)
+
+    const xPos = (offsetX/clientWidth) - 0.5;
+    const yPos = (offsetY/clientHeight) - 0.5;
+
+    const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+    const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+    const modifier = (index) => (index + 1)*1.2
+
+    rightImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: xPos*20*modifier(index),
+            y: yPos*30*modifier(index),
+            rotationY: xPos*40,
+            rotationX: yPos*10
+        })
+    })
+
+    leftImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: -xPos*20*modifier(index),
+            y: -yPos*30*modifier(index),
+            rotationY: -xPos*40,
+            rotationX: -yPos*10
+        })
+    })
+
+    gsap.to('.decor__circle', {
+        duration: 1.7,
+        x: 100*xPos,
+        y: 120*yPos,
+        ease: 'Power4.out'
+    })
 }
 
 function init(){
